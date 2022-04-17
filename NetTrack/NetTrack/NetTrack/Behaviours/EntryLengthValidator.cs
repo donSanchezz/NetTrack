4811141e18d323f0b1df59cb0c637001ee89f1b2
Donovan Saunches <donovansaunches@gmail.com>
2022-04-17 12:30:04 -0500

    Step 1/4 visual

---
+++ NetTrack/NetTrack/NetTrack/Behaviours/EntryLengthValidator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NetTrack.Behaviours
+{
+    public class EntryLengthValidator : Behavior<Entry>
+    {
+        public int MaxLength { get; set; }
+        public int MinLength { get; set; } = 0;
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += OnEntryTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= OnEntryTextChanged;
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var entry = (Entry)sender;
+
+            // if Entry text is longer than valid length  
+            if (entry.Text.Length > this.MaxLength)
+            {
+                string entryText = entry.Text;
+
+                entryText = entryText.Remove(entryText.Length - 1); // remove last char  
+
+                entry.Text = entryText;
+            }
+
+            if (MinLength > 0)
+                if (entry.Text.Length < this.MinLength)
+                {
+                    ((Entry)sender).TextColor = Color.Red;
+                }
+                else
+                    ((Entry)sender).TextColor = Color.Black;
+        }
+    }
+}
